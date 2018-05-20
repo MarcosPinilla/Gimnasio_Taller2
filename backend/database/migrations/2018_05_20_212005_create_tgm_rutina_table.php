3d35e96ec4ce080aa920bf0313ea11cd4ba3430d
Marcos Pinilla <lda_pinigol@hotmail.com>
2018-05-20 17:24:01 -0400

    rutina y rol migrations

---
+++ backend/database/migrations/2018_05_20_212005_create_tgm_rutina_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTgmRutinaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tgm_rutina', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre')->index();
+            $table->string('descripcion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tgm_rutina');
+    }
+}
